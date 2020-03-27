72536f464b1038824ac944f295910b3e3ef04878
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-03-27 14:36:59 -0500

    initial commit

---
+++ src/main/java/com/codeup/whatsupsa/WhatsupsaApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.whatsupsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsupsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsupsaApplication.class, args);
+    }
+
+}
