572b217c87fde9111e948fe47c533b1ae6da21c2
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-03-27 16:58:20 -0500

    created seeder and migration files

---
+++ seeder.sql
@@ -0,0 +1,25 @@
+use eventlister_db;
+
+insert into users (username, email, password) values ('adiss0', 'dadamides0@rambler.ru', 'n8I1Xt8vJlKu');
+insert into users (username, email, password) values ('shynde1', 'hlednor1@umn.edu', 'n1RbqxWWj3Vx');
+insert into users (username, email, password) values ('aizard2', 'glarciere2@geocities.com', '4yoqJ2TC');
+insert into users (username, email, password) values ('gstoacley3', 'pdomeney3@statcounter.com', 'Ye3mk9');
+insert into users (username, email, password) values ('ltersay4', 'rtal4@msu.edu', '4YBdkbKheu');
+insert into users (username, email, password) values ('hanning5', 'agapper5@msn.com', 'iFxwTtMQ');
+insert into users (username, email, password) values ('plongfield6', 'ulamke6@twitter.com', 'EEcZEq5opY');
+insert into users (username, email, password) values ('jjonke7', 'enicol7@comcast.net', 'Bjb6OCGkxx3');
+insert into users (username, email, password) values ('ksharpus8', 'pprobet8@netscape.com', 'hm7HeRgv');
+insert into users (username, email, password) values ('rdot9', 'gknowler9@wikipedia.org', 'i5XqYh9EfZ');
+
+use eventlister_db;
+
+insert into events (title, user_id, description) values ('Ziegfeld Follies', 1, 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.');
+insert into events (title, user_id, description) values ('Event Horizon', 2, 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.');
+insert into events (title, user_id, description) values ('Red Cliff (Chi bi)', 3, 'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.');
+insert into events (title, user_id, description) values ('Chandni Chowk to China', 4, 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.');
+insert into events (title, user_id, description) values ('Waking Up in Reno', 5, 'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.');
+insert into events (title, user_id, description) values ('Primeval', 6, 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.');
+insert into events (title, user_id, description) values ('Jim Jefferies: Fully Functional (EPIX)', 7, 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.');
+insert into events (title, user_id, description) values ('Lone Wolf McQuade', 8, 'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.');
+insert into events (title, user_id, description) values ('Planes: Fire & Rescue', 9, 'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.');
+insert into events (title, user_id, description) values ('Gertrud', 10, 'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.');
