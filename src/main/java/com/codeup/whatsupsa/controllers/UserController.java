d21774c0a9d878c548f2065507ed63cba11fe637
david ortega <david.ortega.dev@gmail.com>
2020-03-30 09:33:06 -0500

    controller and templates start

---
+++ src/main/java/com/codeup/whatsupsa/controllers/UserController.java
@@ -0,0 +1,14 @@
+package com.codeup.whatsupsa.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+    @GetMapping("/register")
+    @ResponseBody
+    public String landing() {
+        return "This is the placeholder for a registration page!";
+    }
+}
