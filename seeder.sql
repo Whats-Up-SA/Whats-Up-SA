91f57fbdaf371c0d84ba79824cbbdc7e5745f445
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-03-30 09:49:45 -0500

    fixed posts table in seeder

--- seeder.sql
+++ seeder.sql
@@ -7,15 +7,15 @@ values ('shynde1', 'hlednor1@umn.edu', 'n1RbqxWWj3Vx');
 
 use eventlister_db;
 
-insert into events (title, user_id, description)
+insert into posts (title, user_id, description)
 values ('Ziegfeld Follies', 1,
         'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.');
-insert into events (title, user_id, description)
+insert into posts (title, user_id, description)
 values ('Events Horizon', 1,
         'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.');
-insert into events (title, user_id, description)
+insert into posts (title, user_id, description)
 values ('Red Cliff (Chi bi)', 2,
         'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.');
-insert into events (title, user_id, description)
+insert into posts (title, user_id, description)
 values ('Chandni Chowk to China', 2,
         'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.');
