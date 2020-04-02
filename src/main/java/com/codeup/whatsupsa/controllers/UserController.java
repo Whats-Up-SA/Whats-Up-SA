8cebdd7e0980d82b2d7fe9fda21bd13a6945aadf
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-04-02 10:30:08 -0500

    in progress

--- src/main/java/com/codeup/whatsupsa/controllers/UserController.java
+++ src/main/java/com/codeup/whatsupsa/controllers/UserController.java
@@ -2,17 +2,11 @@ package com.codeup.whatsupsa.controllers;
 
 import com.codeup.whatsupsa.Repositories.UserRepository;
 import com.codeup.whatsupsa.models.User;
-import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
-
-import javax.validation.constraints.Max;
 
 @Controller
 public class UserController {
