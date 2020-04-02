b580b0217fb853a51c3ee032257f8d99e96bc1b7
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-04-01 19:33:08 -0500

    in progress

--- src/main/java/com/codeup/whatsupsa/controllers/HomeController.java
+++ src/main/java/com/codeup/whatsupsa/controllers/HomeController.java
@@ -1,30 +1,30 @@
-package com.codeup.whatsupsa.controllers;
-
-import com.codeup.whatsupsa.Repositories.EventsRepository;
-import com.codeup.whatsupsa.Repositories.UserRepository;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
-public class HomeController {
-
-    private EventsRepository eventsDao;
-
-    public HomeController(EventsRepository eventsDao) {
-        this.eventsDao = eventsDao;
-    }
-
-    @GetMapping({ "/", "/index" })
-    public String getPosts(Model model) {
-        model.addAttribute("events", eventsDao.findAll());
-        return "/index";
-    }
-
-    @GetMapping("/admin")
-    public String getUnapprovedPosts(Model model) {
-        model.addAttribute("events", eventsDao.findAll());
-        return "/admin";
-    }
-}
+//package com.codeup.whatsupsa.controllers;
+//
+//import com.codeup.whatsupsa.Repositories.EventsRepository;
+//import com.codeup.whatsupsa.Repositories.UserRepository;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//public class HomeController {
+//
+//    private EventsRepository eventsDao;
+//
+//    public HomeController(EventsRepository eventsDao) {
+//        this.eventsDao = eventsDao;
+//    }
+//
+//    @GetMapping("/")
+//    public String getPosts(Model model) {
+//        model.addAttribute("events", eventsDao.findAll());
+//        return "/index";
+//    }
+//
+//    @GetMapping("/admin")
+//    public String getUnapprovedPosts(Model model) {
+//        model.addAttribute("events", eventsDao.findAll());
+//        return "/admin";
+//    }
+//}
