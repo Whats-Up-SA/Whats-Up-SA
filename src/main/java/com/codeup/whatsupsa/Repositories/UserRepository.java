411be0a2ea21cb4f28b5eb50bae29f10969b1e19
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-04-02 20:57:05 -0500

    update still not working

--- src/main/java/com/codeup/whatsupsa/Repositories/UserRepository.java
+++ src/main/java/com/codeup/whatsupsa/Repositories/UserRepository.java
@@ -2,7 +2,6 @@ package com.codeup.whatsupsa.Repositories;
 
 import com.codeup.whatsupsa.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
