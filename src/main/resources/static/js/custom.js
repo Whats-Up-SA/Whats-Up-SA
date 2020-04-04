9e42cfc6a3c3534ac35273fcc797ac81ce02ce93
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-04-04 14:53:21 -0500

    password confirmation added on register.html and update.html

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,5 +1,20 @@
-$(document).ready(function(){
-    $("#submit-btn").click(function(){
-        alert("Thank you for your submission. It will be pending approval by an admin.");
+// alert for submissions
+$(document).ready(function () {
+    $("#submit-btn").click(function () {
+        alert("Thank you for your submission. It will be reviewed by an admin.");
+    });
+});
+
+
+// password confirmation on registration and update page
+$(function () {
+    $(".btnSubmit").click(function () {
+        let password = $(".password").val();
+        let confirmPassword = $(".confirmPassword").val();
+        if (password !== confirmPassword) {
+            alert("Passwords do not match.");
+            return false;
+        }
+        return true;
     });
 });
