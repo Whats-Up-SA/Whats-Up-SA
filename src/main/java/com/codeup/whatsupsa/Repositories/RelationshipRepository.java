ef284c0e111774db6bff5a8d62b9a67f93714f00
david ortega <david.ortega.dev@gmail.com>
2020-04-06 00:34:53 -0500

    friend request button, accept and decline buttons, with db connection

--- src/main/java/com/codeup/whatsupsa/Repositories/RelationshipRepository.java
+++ src/main/java/com/codeup/whatsupsa/Repositories/RelationshipRepository.java
@@ -1,7 +1,16 @@
 package com.codeup.whatsupsa.Repositories;
 
 import com.codeup.whatsupsa.models.Relationship;
+import com.codeup.whatsupsa.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RelationshipRepository extends JpaRepository<Relationship, Long> {
+//    @Query("SELECT a FROM Relationship a WHERE a.userOneID = '%term%' OR a.userOneID = '%term%' AND a.status = 0 AND a.actionUserID <> '%term%'")
+//    List<Relationship> viewPendingRequests(@Param("term") User user);
+
+    @Query("FROM Relationship a WHERE (a.userOneID = ?1 OR a.userTwoID = ?1) AND a.status = 0 AND a.actionUserID <> ?1")
+    List<Relationship> viewPendingRequests(User user);
 }
--- src/main/java/com/codeup/whatsupsa/controllers/UserController.java
+++ src/main/java/com/codeup/whatsupsa/controllers/UserController.java
@@ -1,7 +1,9 @@
 package com.codeup.whatsupsa.controllers;
 
 import com.codeup.whatsupsa.Repositories.EventsRepository;
+import com.codeup.whatsupsa.Repositories.RelationshipRepository;
 import com.codeup.whatsupsa.Repositories.UserRepository;
+import com.codeup.whatsupsa.models.Relationship;
 import com.codeup.whatsupsa.models.User;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,16 +11,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class UserController {
     private UserRepository userDao;
     private PasswordEncoder passwordEncoder;
     private EventsRepository eventDao;
+    private RelationshipRepository relationshipDao;
 
-    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder, EventsRepository eventDao) {
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder, EventsRepository eventDao, RelationshipRepository relationshipDao) {
         this.userDao = userDao;
         this.passwordEncoder = passwordEncoder;
         this.eventDao = eventDao;
+        this.relationshipDao = relationshipDao;
     }
 
     @GetMapping("/all")
@@ -45,23 +54,81 @@ public class UserController {
     public String profile(Model model) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
+        List<Relationship> pendingRequests = relationshipDao.viewPendingRequests(user);
+
+        java.util.List<java.util.Map.Entry<String,Long>> pairList= new java.util.ArrayList<>();
+
+        for (Relationship pendingRequest : pendingRequests) {
+            Map.Entry<String, Long> j = new AbstractMap.SimpleEntry<>(pendingRequest.getActionUserID().getUsername(), pendingRequest.getId());
+            pairList.add(j);
+        }
+
+
+        model.addAttribute("pairList",pairList);
+
+//        model.addAttribute("pendingRequests", relationshipDao.viewPendingRequests(user));
+
         model.addAttribute("events", eventDao.FindEventsByUserID(user.getId()));
-        model.addAttribute("user", userDao.getOne(user.getId()));
+        model.addAttribute("user", user);
+//        model.addAttribute("user", userDao.getOne(user.getId()));
         return "users/profile";
     }
 
+//    @GetMapping("/profile/{id}")
+//    public String getPost(@PathVariable long id, Model model) {
+//        User user = userDao.getOne(id);
+//        model.addAttribute("user", user.getId());
+//        return "events/show";
+//    }
+
     @GetMapping("/profile/{id}")
-    public String getPost(@PathVariable long id, Model model) {
-        User user = userDao.getOne(id);
-        model.addAttribute("user", user.getId());
-        return "events/show";
+    public String otherProfile(@PathVariable long id, Model model) {
+        model.addAttribute("user", userDao.getOne(id));
+        return "users/detail";
     }
 
-//    @GetMapping("/profile/{id}")
-//    public String otherProfile(@PathVariable long id, Model model) {
-//        model.addAttribute("user", userDao.getOne(id));
-//        return "users/otherProfile";
-//    }
+    @PostMapping("/users/detail/fr/{id}")
+    public String friendRequest(@PathVariable long id, Model model, Relationship relationship) {
+        Relationship newRelationship = new Relationship();
+        //checks to see if user is logged in and sets actionUser to them
+        User actionUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User otherUser = userDao.getOne(id);
+        newRelationship.setActionUserID(actionUser);
+        //checks which user's id is lower and sets the lower one to user_one_id
+        if (actionUser.getId() < otherUser.getId()){
+            newRelationship.setUserOneID(actionUser);
+            newRelationship.setUserTwoID(otherUser);
+        } else {
+            newRelationship.setUserOneID(otherUser);
+            newRelationship.setUserTwoID(actionUser);
+        }
+        //sets relationship status to pending
+        newRelationship.setStatus(0);
+        //saves relationship in database
+
+        //        Relationship testRelationship = new Relationship(otherUser, actionUser, actionUser, 0);
+        relationshipDao.save(newRelationship);
+
+        return "redirect:/index";
+    }
+
+    @PostMapping("/profile/{id}/approve")
+    public String approveFriend(@PathVariable long id){
+        Relationship r = relationshipDao.getOne(id);
+        r.setStatus(1);
+        r.setActionUserID((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        relationshipDao.save(r);
+        return"redirect:/profile";
+    }
+
+    @PostMapping("/profile/{id}/decline")
+    public String declineFriend(@PathVariable long id){
+        Relationship r = relationshipDao.getOne(id);
+        r.setStatus(2);
+        r.setActionUserID((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        relationshipDao.save(r);
+        return"redirect:/profile";
+    }
 
     @GetMapping("/update")
     public String showUpdateForm(Model model) {
